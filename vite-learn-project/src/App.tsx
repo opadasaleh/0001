14f7df0720a91257eed31aae28e566a3aec3f5e6
opada <opadasadleh@gmail.com>
2025-07-22 23:41:44 +0300

    add aboutpage func

--- vite-learn-project/src/App.tsx
+++ vite-learn-project/src/App.tsx
@@ -1,11 +1,25 @@
 import { add, format } from "date-fns"
 
+function MyButton() {
+  return < button>i'm a button</button>
+}
+function AboutPage() {
+  return (
+    <>
+      <h1>hello welcome to AboutPage</h1>
+      <br />
+      <p>the page thast will discripe about the site</p>
+    </>
+  )
+}
 function App() {
 
   return (
     <>
       Tomorrow's Date: {format(add(new Date(), { days: 1 }), "do MMMM yyyy")}
       Tomorrow's Date: {format(add(new Date(), { days: 1 }), "do MMMM yyyy")}
+      <MyButton />
+      <AboutPage/>
     </>
   )
 }
