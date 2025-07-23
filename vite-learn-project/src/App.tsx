0c1abee60e442d16bc7ae012ec6ee9b7cf353cb1
opada <opadasadleh@gmail.com>
2025-07-23 19:29:23 +0300

    fix:delete all paragraph

--- vite-learn-project/src/App.tsx
+++ vite-learn-project/src/App.tsx
@@ -1,27 +1,9 @@
-import { add, format } from "date-fns"
+import Login from "./Login"
 
-function MyButton() {
-  return < button>i'm a button</button>
-}
-function AboutPage() {
-  return (
-    <>
-      <h1>hello welcome to AboutPage</h1>
-      <br />
-      <p>the page thast will discripe about the site</p>
-      <p>the page thast will discripe about the site</p>
-      <p>the page thast will discripe about the site</p>
-    </>
-  )
-}
 function App() {
-
   return (
     <>
-      Tomorrow's Date: {format(add(new Date(), { days: 1 }), "do MMMM yyyy")}
-      Tomorrow's Date: {format(add(new Date(), { days: 1 }), "do MMMM yyyy")}
-      <MyButton />
-      <AboutPage/>
+      <Login />
     </>
   )
 }
